4ffc29614c29bdaf8961fe2fe51ba271de943657
colt <necromanga@gmail.com>
2017-03-02 11:57:00 +0100

    Initial commit

---
+++ app/src/main/java/com/koltinjo/venner/ActivityMain.java
@@ -0,0 +1,14 @@
+package com.koltinjo.venner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
